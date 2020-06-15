c38a676fb8534bc4a52ac059ba44c720c057dc76
yashkhem1 <yashkhem1@gmail.com>
2020-06-15 08:44:14 +0530

    evaluation for missing data imputation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.parsing import Options
 from src.train import train_clf, train_sr, train_sr_gan, train_imp, train_imp_gan
-from src.evaluate import evaluate_clf, evaluate_ecg_sr
+from src.evaluate import evaluate_clf, evaluate_ecg_sr, evaluate_ecg_imp
 
 if __name__ == "__main__":
     opt = Options().parse()
@@ -10,9 +10,13 @@ if __name__ == "__main__":
         if opt.model_type == 'clf':
             evaluate_clf(opt)
 
-        elif opt.model_type == 'sr' or opt.model_type == 'sr_gan':
+        elif opt.model_type in ['sr','sr_gan']:
             if opt.data_type=='ecg':
                 evaluate_ecg_sr(opt)
+
+        elif opt.model_type in ['imp','imp_gan']:
+            if opt.data_type=='ecg':
+                evaluate_ecg_imp(opt)
 
     elif opt.model_type == 'clf':
         train_clf(opt)
