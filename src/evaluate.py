6606f34ccdea5acf6f4f41060691b2f3620b2791
yashkhem1 <yashkhem1@gmail.com>
2020-06-20 21:55:32 +0530

    Updated Evaluation for Classification

--- src/evaluate.py
+++ src/evaluate.py
@@ -8,10 +8,37 @@ def evaluate_clf(opt):
     C = load_model(opt.classifier_path)
     test_X,test_Y = read_test_data(opt.data_type)
     if opt.data_type=='ecg':
-        test_X = test_X[:,::opt.sampling_ratio,:]
-    if opt.use_sr_clf:
-        G = load_model(opt.model_path)
-        test_X = G.predict(test_X,batch_size=opt.test_batch_size,verbose=1)
+        if opt.sampling_ratio != 1:
+            test_X = test_X[:,::opt.sampling_ratio,:]
+            if opt.use_sr_clf:
+                G = load_model(opt.model_path)
+                test_X = G.predict(test_X,batch_size=opt.test_batch_size,verbose=1)
+
+        if opt.prob != 0:
+            np.random.seed(opt.seed)
+            indices = np.arange(192)
+            n_missing = int(opt.prob * 192)
+            test_X_m = np.zeros(test_X.shape)
+            test_mask = np.ones(test_X.shape)
+            for i, data in enumerate(test_X):
+                if opt.cont:
+                    missing_start = np.random.randint(0, int((1 - opt.prob) * 192) + 1)
+                    missing_indices = np.arange(missing_start, missing_start + n_missing)
+                else:
+                    missing_indices = np.random.choice(indices, n_missing, replace=False)
+                test_X_m[i] = data
+                test_X_m[i][missing_indices] = 0
+                test_mask[i][missing_indices] = 0
+
+            if opt.use_imp_clf:
+                G = load_model(opt.imp_model)
+                test_X_m_mask = np.concatenate([test_X_m, test_mask], axis=-1)
+                x_pred = G.predict(test_X_m_mask, batch_size=opt.batch_size, verbose=1)
+                test_X = test_X_m * test_mask + x_pred * (1 - test_mask)
+
+            else:
+                test_X = test_X_m
+
     y_true = np.argmax(test_Y,axis=1)
     y_pred = np.argmax(C.predict(test_X,batch_size=opt.test_batch_size,verbose=1),axis=1)
     accuracy = accuracy_score(y_true,y_pred)
